b9ef23655e3d33eeeb2909376ca94ad1b40ebaa6
Sebastian Ortiz Serna <sortizs@eafit.edu.co>
2019-08-02 22:22:44 -0500

    Test project and test configuration for Insomnia

---
+++ Prueba_TEV/Prueba_TEV.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Prueba_TEV.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Prueba_TEV.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cf6820dc-8679-4454-9018-7b72974c1bd2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Prueba_TEV/Prueba_TEV.Tests/UserTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Http.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Prueba_TEV.Controllers;
+using Prueba_TEV.Models;
+
+namespace Prueba_TEV.Tests
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void GetReturnsUser()
+        {
+            var mockRepository = new Mock<IUserRepository>();
+            mockRepository.Setup(x => x.GetUser(42)).Returns(new User { ID = 1 });
+
+            var controller = new UserController(mockRepository.Object);
+
+            IHttpActionResult actionResult = controller.Get(42);
+            var contentResult = actionResult as OkNegotiatedContentResult<User>;
+
+            Assert.IsNotNull(contentResult);
+            Assert.IsNotNull(contentResult.Content);
+            Assert.AreEqual(1, contentResult.Content.ID);
+        }
+
+        [TestMethod]
+        public void GetReturnsNotFound()
+        {
+            var mockRepository = new Mock<IUserRepository>();
+            var controller = new UserController(mockRepository.Object);
+
+            IHttpActionResult actionResult = controller.Get(99);
+
+            Assert.IsInstanceOfType(actionResult, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void PostSetsLocationHeader()
+        {
+            User user = new User()
+            {
+                ID = 31,
+                Name = "Andres",
+                LastName = "Arango",
+                Address = "Medellin, Colombia",
+                CreateDate = DateTime.Today,
+                UpdateDate = DateTime.Today
+            };
+
+            var mockRepository = new Mock<IUserRepository>();
+            var controller = new UserController(mockRepository.Object);
+
+            IHttpActionResult actionResult = controller.Post(user);
+            var createdResult = actionResult as CreatedAtRouteNegotiatedContentResult<User>;
+
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual("DefaultApi", createdResult.RouteName);
+            Assert.AreEqual(31, createdResult.RouteValues["ID"]);
+        }
+
+        [TestMethod]
+        public void PutReturnsUser()
+        {
+            User user = new User()
+            {
+                ID = 1,
+                Name = "Sebastian",
+                LastName = "Ortiz Serna",
+                Address = "Medellin, Colombia",
+                UpdateDate = DateTime.Today
+            };
+
+            var mockRepository = new Mock<IUserRepository>();
+            mockRepository.Setup(x => x.UpdateUser(1, user)).Returns(user);
+
+            var controller = new UserController(mockRepository.Object);
+
+            IHttpActionResult actionResult = controller.Put(1, user);
+            var contentResult = actionResult as OkNegotiatedContentResult<User>;
+
+            Assert.IsNotNull(contentResult);
+            Assert.IsNotNull(contentResult.Content);
+            Assert.AreEqual(1, contentResult.Content.ID);
+        }
+
+        [TestMethod]
+        public void DeleteReturnsOk()
+        {
+            var mockRepository = new Mock<IUserRepository>();
+            var controller = new UserController(mockRepository.Object);
+
+            IHttpActionResult actionResult = controller.Delete(2);
+
+            Assert.IsInstanceOfType(actionResult, typeof(OkResult));
+        }
+    }
+}
